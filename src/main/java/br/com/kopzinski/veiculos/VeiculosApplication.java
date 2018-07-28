9064a2bfa6b7234eb2ebb768a21abb75d8819c9f
mellop <Paulo.Mello@tabcorp.com.au>
2018-07-28 09:57:49 -0300

    chore: create basic app setup

---
+++ src/main/java/br/com/kopzinski/veiculos/VeiculosApplication.java
@@ -0,0 +1,12 @@
+package br.com.kopzinski.veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculosApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/kopzinski/veiculos/VeiculosApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.kopzinski.veiculos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VeiculosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
